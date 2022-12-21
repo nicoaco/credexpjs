eafae258c1dd1e0cf9b79e2286733d1be314cdc3
nicoaco <nicoaco@gmail.com>
2022-12-21 13:13:42 +0000

    creacion https, certificados y redirección 80 a 443

--- src/app.js
+++ src/app.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs');
+const https = require('https');
 const mainRoutes = require('./routes/main');
 const asociadosRoutes = require('./routes/asociados');
 
 app.use(express.static(path.resolve(__dirname,'../public')));
 app.set('view engine', 'ejs');
 
-app.listen(3001, console.log('Server running on port 3001'));
+app.enable('trust proxy')
+app.use(function(request, response, next) {
+
+    if (process.env.NODE_ENV != 'development' && !request.secure) {
+       return response.redirect("https://" + request.headers.host + request.url);
+    }
+
+    next();
+})
+
+app.listen(80, console.log('Server running on port 80'));
+
+https.createServer({
+    cert: fs.readFileSync('/etc/letsencrypt/live/credisol.coop.ar/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/credisol.coop.ar/privkey.pem')
+  },app).listen(443, function(){
+     console.log('Servidor https correindo en el puerto 443');
+ });
 
 app.use('/', mainRoutes);
 app.use('/asociados', asociadosRoutes);
+
